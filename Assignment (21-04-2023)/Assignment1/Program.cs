f88161b0bafc6bd4890ab777994d9f0eb0a2e225
sivakumarkalluri <sivakumarreddykalluri@gmail.com>
2023-04-21 17:47:18 +0530

    updated assignments of 21-04-2023

---
+++ Assignment (21-04-2023)/Assignment1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius of Circle: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            Circle circle = new Circle(radius);
+            Console.Write("Enter length of Rectangle: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter breadth of Rectangle: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Rectangle rect = new Rectangle(l,b);
+            Console.Write("Enter Side of Square: ");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Square sq = new Square(s);
+
+            Console.WriteLine("Area of the Circle is : " + circle.area());
+            Console.WriteLine("Area of the Rectangle is : " + rect.area());
+            Console.WriteLine("Area of the Square is : " + sq.area());
+
+        }
+    }
+
+    abstract class Calculate
+    {
+
+        public abstract double area();
+    }
+
+    class Circle : Calculate
+    {
+        double radius;
+ 
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double area()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+        
+    }
+
+    class Rectangle : Calculate
+    {
+        double length, breadth;
+        public Rectangle( double length,double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public override double area()
+        {
+            return this.length * this.breadth;
+        }
+
+    }
+
+    class Square : Calculate
+    {
+
+        double length;
+
+        public Square(double length)
+        {
+            this.length = length;
+        }
+        public override double area()
+        {
+            return this.length * this.length;
+        }
+
+    }
+}
---
+++ Assignment (21-04-2023)/Assignment2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //************ Input Data  ***********
+            Console.Write("Enter Number of Employees : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+           
+            //********** Creating Array of Objects ********
+
+            SalesAssociate[] emps=new SalesAssociate[size];
+
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write("Enter Salary of Employee" + (i + 1)+" : ");
+                int salary = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Monthly Sales of Employee" + (i + 1) + " : ");
+                int sales= Convert.ToInt32(Console.ReadLine());
+                emps[i] = new SalesAssociate(salary,sales);
+            }
+
+            // ********* Displaying Output Data ****************
+
+            Console.WriteLine("\n********* Calculated Data *********\n");
+            int count = 1;
+            foreach(var emp in emps){
+                Console.WriteLine("Total Salary (including Bonus) of Employee" + count+" is :"+emp.SalesBonus());
+                count++;
+
+
+            }
+            
+        }
+    }
+
+
+    //*********** Parent Class ***********
+    class Employee
+    {
+        int salesNumber;
+
+        
+        public  void SetNumberOfSales(int num)
+        {
+            this.salesNumber = num;
+
+        }
+        public int GetNumberOfSales()
+        {
+            return this.salesNumber;
+        }
+
+
+
+        
+    }
+
+    //********** Child Class *******
+    class SalesAssociate:Employee
+    {
+        int Salary;
+        float Bonus = 0;
+        public SalesAssociate(int salary,int sales)
+        {
+            this.Salary = salary;
+            SetNumberOfSales(sales);
+           
+        }
+
+        public float SalesBonus()
+        {
+            int numSales = GetNumberOfSales();
+
+            if (numSales >= 10 && numSales < 20)
+                this.Bonus = this.Salary + (((float)5 / 100) * this.Salary);
+
+            else if (numSales >= 20 && numSales < 30)
+                this.Bonus = this.Salary + (((float)10 / 100) * this.Salary);
+
+            else if (numSales >= 30)
+                this.Bonus = this.Salary + (((float)20 / 100) * this.Salary);
+
+            else
+                this.Bonus = this.Salary;
+
+            
+            return this.Bonus;
+
+        }
+
+    }
+}
